dbc4551006ac761b83f6a5026ecdd98c1711bb1e
Aynur Maksutov <maksutov_aynur@mail.ru>
2024-02-25 05:54:36 +0300

    fx

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ if [ -z "$FA_PROXY_ACCESS_PASSWORD" ]; then echo "Error: FA_PROXY_ACCESS_PASSWOR
 if [ -z "$FA_PROXY_APP_PORT" ]; then echo "Error: FA_PROXY_APP_PORT not set. Exit"; exit 1; fi
 if [ -z "$FA_PROXY_REDIRECT_URL" ]; then echo "Error: FA_PROXY_REDIRECT_URL not set. Exit"; exit 1; fi
 
-DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 COMPOSE_PROJECT_NAME=${FA_PROXY_PROJECT} docker-compose up --build -d
+docker-compose up --build -d
--- stop.sh
+++ stop.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 source ./env.sh
-
-DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 COMPOSE_PROJECT_NAME=${FA_PROXY_PROJECT} docker-compose down
+docker-compose down
